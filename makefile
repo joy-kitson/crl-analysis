2b57e87f7d03793beb876c296891d26a04fe11cb
Joy Kitson <jkitson@umd.edu>
2021-05-12 14:42:23 -0400

    now parsing crls and outputting to csvs

--- makefile
+++ makefile
@@ -1,2 +1,35 @@
+CRLS=gds5-16.crl CloudflareIncECCCA-3.crl GTS1O1core.crl gsr2.crl root-r2.crl
+CSVS=$(subst .crl,.csv,$(CRLS))
+
+.PHONY:all
+all: csvs
+
+# Downloads all crls
+.PHONY:crls
+crls: $(CRLS)
+
 gds5-16.crl:
 	wget http://crl.godaddy.com/gds5-16.crl
+
+CloudflareIncECCCA-3.crl:
+	wget http://crl4.digicert.com/CloudflareIncECCCA-3.crl
+
+GTS1O1core.crl:
+	wget http://crl.pki.goog/GTS1O1core.crl
+
+gsr2.crl:
+	wget http://crl.pki.goog/gsr2/gsr2.crl
+
+root-r2.crl:
+	wget http://crl.globalsign.net/root-r2.crl
+
+.PHONY:csvs
+csvs: $(CSVS)
+
+%.csv: %.crl parse-crl.py
+	./parse-crl.py -i $< > $@
+
+# Removes all generated files
+.PHONY:clean
+clean:
+	rm $(CRLS) $(CSVS)
--- parse-crl.py
+++ parse-crl.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python3
 
-print('hi')
+import argparse
+import OpenSSL
+import time
+import calendar
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Optional/named arguments
+    parser.add_argument('-i', '--input', default='gds5-16.crl')
+    
+    return parser.parse_args()
+
+def read_crl(path, is_asn1=True):
+    with open(path, 'rb') as f:
+        raw_crl = f.read()
+
+    if is_asn1:
+        return OpenSSL.crypto.load_crl(OpenSSL.crypto.FILETYPE_ASN1, raw_crl)
+    else:
+        return OpenSSL.crypto.load_crl(OpenSSL.crypto.FILETYPE_PEM, raw_crl)
+
+def try_decode(byte_string):
+    try:
+        return byte_string.decode('UTF-8')
+    except:
+        return None
+
+def main():
+    args = parse_args()
+
+    crl = read_crl(args.input)
+    issuer = crl.get_issuer().organizationName
+
+    print('issuer,serial,reason,date')
+    for r in crl.get_revoked():
+        serial = try_decode(r.get_serial())
+        reason = try_decode(r.get_reason())
+        rev_date = try_decode(r.get_rev_date())
+        rev_date = calendar.timegm(time.strptime(rev_date, '%Y%m%d%H%M%SZ'))
+        print(f'"{issuer}",{serial},"{reason}",{rev_date}')
+
+if __name__ == '__main__':
+    main()
